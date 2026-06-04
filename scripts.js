2d500b91b4f1e0be683482ca0b2d429890141948
yazanalahamdan-ctrl <yazanalahamdan@gmail.com>
2026-06-04 22:52:37 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener('click',e=>{const id=a.getAttribute('href');if(id.length>1){e.preventDefault();document.querySelector(id)?.scrollIntoView({behavior:'smooth'});}})});});
